86acb370375aad6104370f99b10757fbd3a2bf86
Aditya Nugroho <84896146+adityanugrohoe@users.noreply.github.com>
2024-09-24 09:21:15 +0700

    Delete weekday_selector directory

--- weekday_selector/coverage.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-flutter test --coverage
-genhtml coverage/lcov.info -o coverage/html
-
-echo 'Run the following command to open the coverage report in your browser'
-echo '> open coverage/html/index.html'
